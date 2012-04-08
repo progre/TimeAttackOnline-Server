1a5269fdea65720e0fe4d9be17e045ababee7006
ide user ide_progre <djyayutto@gmail.com>
2012-04-08 06:41:18 +0000

    add commands

--- web.js
+++ web.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var server = express.createServer();
+server.get('/exists', function(req, res) {
+    res.send(req.param('pass-phrase'));
+});
 server.get('/add', function(req, res) {
-    res.send('add command');
+    res.send(req.param('pass-phrase'));
+    res.send(req.param('title'));
+    res.send(req.param('date'));
 });
 var port = process.env.PORT || process.env.C9_PORT;
 server.listen(port, function() {
