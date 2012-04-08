41aad5bf3563a842ccf6aa001dfaa4ca36ec4c28
ide user ide_progre <djyayutto@gmail.com>
2012-04-08 09:58:47 +0000

    update

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
+console.log('wakeup...');
+//var conString = process.env.DATABASE_URL || "tcp://<ユーザー名>:<パスワード>@localhost:5432/testdb";
+//console.log('db on: ' + conString);
 var express = require('express');
 var server = express.createServer();
 server.get('/exists', function(req, res) {
-    res.send(req.param('pass-phrase'));
+    req.param('pass-phrase', '');
+    var message = JSON.stringify({exists:false});
+    res.send(message);
 });
 server.get('/add', function(req, res) {
-    res.send(req.param('pass-phrase'));
-    res.send(req.param('title'));
-    res.send(req.param('date'));
+    req.param('pass-phrase');
+    res.send(JSON.stringify({result:true}));
 });
 var port = process.env.PORT || process.env.C9_PORT;
 server.listen(port, function() {
