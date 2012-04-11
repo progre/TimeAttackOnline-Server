ff9fa7811c388de9c2d27d48c69a551473e91bf7
ide user ide_progre <djyayutto@gmail.com>
2012-04-11 09:26:47 +0000

    under construction...

--- web.js
+++ web.js
@@ -1,18 +1,7 @@
-console.log('wakeup...');
-//var conString = process.env.DATABASE_URL || "tcp://<ユーザー名>:<パスワード>@localhost:5432/testdb";
-//console.log('db on: ' + conString);
+var initializes = require('./initializes');
 var express = require('express');
-var server = express.createServer();
-server.get('/exists', function(req, res) {
-    req.param('pass-phrase', '');
-    var message = JSON.stringify({exists:false});
-    res.send(message);
-});
-server.get('/add', function(req, res) {
-    req.param('pass-phrase');
-    res.send(JSON.stringify({result:true}));
-});
-var port = process.env.PORT || process.env.C9_PORT;
-server.listen(port, function() {
-    console.log("Listening on " + port);
-});
+var pg = require('pg');
+
+console.log('wakeup...');
+initializes.initialize(express, pg);
+console.log('main-process end.');
