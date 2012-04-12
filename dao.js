d4e77ecbf3d846ef5db9294f820166232f5f11a3
ide user ide_progre <djyayutto@gmail.com>
2012-04-12 08:49:07 +0000

    under construction...

---
+++ dao.js
@@ -0,0 +1,55 @@
+var Dao = function (client) {
+    /** @private */
+    this.client_ = client;
+};
+
+Dao.getTimeAttackEvent = function (passPhrase) {
+    var rows = [];
+    var query = this.client_.query('SELECT * FROM time_attack_events WHERE pass_phrase = $1', [passPhrase]);
+    query.on('error', function (error) {
+        console.log('PG: Error: ' + error.toString());
+    });
+    query.on('row', function (row) {
+        rows.push(row);
+    });
+    // 最後の行に到達したときの処理
+    query.on('end', function (row, error) {
+        console.log('PG: End: ' + row.toString() + error.toString());
+    });
+};
+
+Dao.addTimeAttackEvent = function (passPhrase) {
+    var rows = [];
+    var query = this.client_.query('SELECT * FROM time_attack_events WHERE pass_phrase = $1', [passPhrase]);
+    query.on('error', onError);
+    query.on('row', function (row) {
+        rows.push(row);
+    });
+    // 最後の行に到達したときの処理
+    query.on('end', function (row, error) {
+        console.log('PG: End: ' + row.toString() + error.toString());
+    });
+};
+
+/** @private */
+Dao.existsTableAsync_ = function (tableName, callback) {
+    var query = this.client_.query('SHOW TABLE STATUS FROM $1', [tableName]);
+    query.on('error', onError);
+    var exists = false;
+    query.on('row', function (row) {
+        exists = true;
+    });
+    query.on('end', function (row, error) {
+        if (exists) {
+            throw new Error('attayo');
+        }
+        console.log('PG: End: ' + row.toString() + error.toString());
+        callback(exists);
+    });
+};
+
+function onError(error) {
+    throw new Error('PG: Error: ' + error.toString());
+}
+
+exports.Dao = Dao;
