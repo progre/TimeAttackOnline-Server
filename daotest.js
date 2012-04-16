d599fe059e81c25c5e74bf2ad51a0baf104fd736
ide user ide_progre <djyayutto@gmail.com>
2012-04-16 05:32:03 +0000

    under construction...

--- daotest.js
+++ daotest.js
@@ -4,6 +4,9 @@ var DaoTest = function (dao) {
 
 DaoTest.prototype.test = function (response) {
     var message = '<p>\nStart dao test-->\n';
+    if (this.dao_ === undefined || this.dao_ === null){
+        message += 'dao is ' + this.dao_;
+    }
     this.dao_.existsTableAsync(this.dao_.client_, "test_table", function (exists) {
         message += 'table exists: ' + exists;
         message += '<--end of dao test</p>';
